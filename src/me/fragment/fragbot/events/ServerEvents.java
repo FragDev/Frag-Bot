49dfb5de8bd3aee59c5c3206943c1a5dca04a136
Fragment <clemcailly@gmail.com>
2021-06-17 11:19:21 +0200

    Welcome message

--- src/me/fragment/fragbot/events/ServerEvents.java
+++ src/me/fragment/fragbot/events/ServerEvents.java
@@ -17,17 +17,25 @@ public class ServerEvents extends ListenerAdapter {
 	public void onGuildMemberJoin(GuildMemberJoinEvent event) {
 		event.getGuild().getTextChannelsByName("welcome", false).forEach(textChannel -> {
 			textChannel
-					.sendMessage(new EmbedBuilder().setColor(new Color(47, 49, 54)).setTitle("Welcome to Fragment - Java Dev Station !")
-							.setDescription("**👋 Welcome to " + event.getMember().getAsMention() + "**").setThumbnail(event.getUser().getAvatarUrl())
-							.setFooter("Frag'Bot say welcome to " + event.getUser().getName(), FragBot.getJda().getSelfUser().getAvatarUrl()).build())
-					.setActionRow(Button.of(ButtonStyle.PRIMARY, "welcome", "Say Welcome", Emoji.fromMarkdown("👋"))).queue();
+					.sendMessage(new EmbedBuilder().setColor(new Color(47, 49, 54))
+							.setImage(FragBot.getJda().getSelfUser().getAvatarUrl())
+							.setTitle("Welcome to Fragment - Java Dev Station !")
+							.setDescription("**👋 Welcome to " + event.getMember().getAsMention() + "**")
+							.setThumbnail(event.getUser().getAvatarUrl())
+							.setFooter("Frag'Bot say welcome to " + event.getUser().getName(),
+									FragBot.getJda().getSelfUser().getAvatarUrl())
+							.build())
+					.setActionRow(Button.of(ButtonStyle.PRIMARY, "welcome", "Say Welcome", Emoji.fromMarkdown("👋")))
+					.queue();
 		});
 	}
 
 	@Override
 	public void onButtonClick(ButtonClickEvent event) {
 		if (event.getButton().getId().equalsIgnoreCase("welcome") && event.getChannel().getName().equals("welcome")) {
-			event.getMessage().editMessage(new EmbedBuilder(event.getMessage().getEmbeds().get(0)).setDescription("a").build()).queue();
+			event.getMessage()
+					.editMessage(new EmbedBuilder(event.getMessage().getEmbeds().get(0)).setDescription("a").build())
+					.queue();
 			event.deferEdit().queue();
 		}
 	}
